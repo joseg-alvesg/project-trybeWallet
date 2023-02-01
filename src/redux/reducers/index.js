d2b39889140d251b12ab6106c0dec72207773152
joseg-alvesg <cinhoalves85@gmail.com>
2023-02-01 15:47:19 -0300

    configuração do redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,25 @@
-// import user from './user';
-// import wallet from './wallet';
+import { combineReducers } from 'redux';
 
-// Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+const INITIAL_STATE = {
+  user: {
+    email: '', // string que armazena o email da pessoa usuária
+    password: '', // string que armazena a senha da pessoa usúaria
+  },
+  wallet: {
+    currencies: [], // array de string
+    expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
+    editor: false, // valor booleano que indica de uma despesa está sendo editada
+    idToEdit: 0, // valor numérico que armazena o id da despesa que esta sendo editada
+  },
+};
+
+export const userReducer = (state = INITIAL_STATE, { type }) => {
+  switch (type) {
+  default:
+    return state;
+  }
+};
+
+const rootReducer = combineReducers({ userReducer });
+
+export default rootReducer;
